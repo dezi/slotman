cbafab79383c9c75b636072751594bca5c7cd0a1
dezi <dezi@kappa-mm.de>
2025-01-19 15:09:55 +0100

    GC9A01 display.

--- soft/golang/slotman/roles/slotman/main.go
+++ soft/golang/slotman/roles/slotman/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"slotman/services/impl/provider"
-	"slotman/things/galaxycore/gc9a01"
 	"slotman/utils/daemon"
 	"slotman/utils/exitter"
 )
@@ -16,7 +15,7 @@ func startup() {
 	//_ = logger.StartService()
 	_ = provider.StartService()
 
-	gc9a01.TestDisplay()
+	//gc9a01.TestDisplay()
 
 	_ = exitter.StartService()
 

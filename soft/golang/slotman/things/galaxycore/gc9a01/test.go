ab8dd6ec988293d9b29b4e6edc063c1f720fe0f6
dezi <dezi@kappa-mm.de>
2025-01-19 11:33:36 +0100

    GC9A01 display.

--- soft/golang/slotman/things/galaxycore/gc9a01/test.go
+++ soft/golang/slotman/things/galaxycore/gc9a01/test.go
@@ -34,52 +34,53 @@ func TestDisplay() {
 
 	color := make([]byte, 3)
 	line := make([]byte, 3*240*240)
+	_ = line
 
 	for {
 
 		color[0] = byte(rand.Int31())
 		color[1] = byte(rand.Int31())
 
-		//for x := 0; x < 240; x++ {
-		//	for y := 0; y < 240; y++ {
-		//		if x < y {
-		//			color[2] = 0xFF
-		//		} else {
-		//			color[2] = 0x00
-		//		}
-		//		if x == 0 && y == 0 {
-		//			_ = gc9a01.WriteMem(color)
-		//		} else {
-		//			_ = gc9a01.WriteMemCont(color)
-		//		}
-		//	}
-		//}
-
 		for x := 0; x < 240; x++ {
-
-			off := 0
-
 			for y := 0; y < 240; y++ {
 				if x < y {
 					color[2] = 0xFF
 				} else {
 					color[2] = 0x00
 				}
-
-				line[off] = color[0]
-				off++
-				line[off] = color[1]
-				off++
-				line[off] = color[2]
-				off++
-			}
-
-			if x == 0 {
-				_ = gc9a01.WriteMem(line)
-			} else {
-				_ = gc9a01.WriteMemCont(line)
+				if x == 0 && y == 0 {
+					_ = gc9a01.WriteMem(color)
+				} else {
+					_ = gc9a01.WriteMemCont(color)
+				}
 			}
 		}
+
+		//for x := 0; x < 240; x++ {
+		//
+		//	off := 0
+		//
+		//	for y := 0; y < 240; y++ {
+		//		if x < y {
+		//			color[2] = 0xFF
+		//		} else {
+		//			color[2] = 0x00
+		//		}
+		//
+		//		line[off] = color[0]
+		//		off++
+		//		line[off] = color[1]
+		//		off++
+		//		line[off] = color[2]
+		//		off++
+		//	}
+		//
+		//	if x == 0 {
+		//		_ = gc9a01.WriteMem(line)
+		//	} else {
+		//		_ = gc9a01.WriteMemCont(line)
+		//	}
+		//}
 
 		//_ = gc9a01.WriteMem(line)
 

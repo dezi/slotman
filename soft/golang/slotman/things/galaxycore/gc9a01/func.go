af07a4d0f2f395d3cd0586a45e64f51bf001b149
dezi <dezi@kappa-mm.de>
2025-01-19 11:10:31 +0100

    GC9A01 display.

--- soft/golang/slotman/things/galaxycore/gc9a01/func.go
+++ soft/golang/slotman/things/galaxycore/gc9a01/func.go
@@ -35,8 +35,7 @@ func (se *GC9A01) OpenSensor() (err error) {
 
 	_ = se.spi.SetMode(0)
 	_ = se.spi.SetBitsPerWord(8)
-	_ = se.spi.SetSpeed(80000000)
-	//_ = se.spi.SetSpeed(250000000)
+	_ = se.spi.SetSpeed(100000000)
 
 	se.dcPin, err = gpio.GetPin(25)
 	if err != nil {

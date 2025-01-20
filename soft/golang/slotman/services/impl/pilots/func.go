2077f18ca477e77899c65f715bf5e35374e6733d
dezi <dezi@kappa-mm.de>
2025-01-20 17:18:59 +0100

    Pilot stuff.

--- soft/golang/slotman/services/impl/pilots/func.go
+++ soft/golang/slotman/services/impl/pilots/func.go
@@ -1,16 +1,11 @@
 package pilots
 
 import (
-	"fmt"
-	"golang.org/x/image/draw"
 	"image"
-	"image/png"
-	"path/filepath"
 	"slotman/goodies/imaging"
 	"slotman/services/type/slotman"
 	"slotman/utils/log"
 	"slotman/utils/simple"
-	"strings"
 )
 
 func (sv *Service) UpdatePilot(pilot *slotman.Pilot) {
@@ -69,28 +64,4 @@ func (sv *Service) UpdatePilot(pilot *slotman.Pilot) {
 		delete(sv.pilotProfileFull, appUuid)
 		delete(sv.pilotProfileSmall, appUuid)
 	}
-}
-
-func GetScaledPilotImage(pilot string, size int) (img *image.RGBA, err error) {
-
-	file := fmt.Sprintf("profile-%s.jpg", strings.ToLower(pilot))
-
-	input, err := embedFs.Open(filepath.Join("embeds", file))
-	if err != nil {
-		log.Cerror(err)
-		return
-	}
-
-	defer func() { _ = input.Close() }()
-
-	src, err := png.Decode(input)
-	if err != nil {
-		log.Cerror(err)
-		return
-	}
-
-	img = image.NewRGBA(image.Rect(0, 0, size, size))
-	draw.NearestNeighbor.Scale(img, img.Rect, src, src.Bounds(), draw.Over, nil)
-
-	return
 }

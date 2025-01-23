6c69175f6966db5deec3517466b75f9d7192ca18
dezi <dezi@kappa-mm.de>
2025-01-23 13:28:15 +0100

    Git ignores.

--- soft/golang/slotman/services/impl/pilots/embeds.go
+++ soft/golang/slotman/services/impl/pilots/embeds.go
@@ -3,11 +3,11 @@ package pilots
 import "embed"
 
 //go:embed embeds/profile-dennis.jpg
-//go:embed embeds/profile-fernando-alonso.jpg
+//go:embed embeds/fernando-alonso-aston-martin-r.jpg
 //go:embed embeds/profile-kim.jpg
-//go:embed embeds/profile-lewis-hamilton.jpg
+//go:embed embeds/lewis-hamilton-mercedes.jpg
 //go:embed embeds/profile-lukas.jpg
-//go:embed embeds/profile-max-verstappen.jpg
+//go:embed embeds/max-verstappen-red-bull-racing.jpg
 //go:embed embeds/profile-omar.jpg
 //go:embed embeds/profile-patrick.jpg
 //go:embed embeds/profile-susi.jpg

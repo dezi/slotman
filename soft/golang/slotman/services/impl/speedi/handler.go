2ce03781718bec8b9ffef18b620a0706f4450b15
dezi <dezi@kappa-mm.de>
2025-02-06 23:41:56 +0100

    Tacho and speedi proxies.

--- soft/golang/slotman/services/impl/speedi/handler.go
+++ soft/golang/slotman/services/impl/speedi/handler.go
@@ -156,7 +156,9 @@ func (sv *Service) OnADConversion(thing things.Thing, input int, value uint16) {
 
 func (sv *Service) OnCurrentSpeed(track int, speedPcc float64) {
 
-	log.Printf("OnCurrentSpeed track=%d speedPcc=%0.1f", track, speedPcc)
+	if speedPcc > 5.0 {
+		log.Printf("OnCurrentSpeed track=%d speedPcc=%0.1f", track, speedPcc)
+	}
 
-	_ = sv.sdo.SetSpeed(track, speedPcc, true)
+	//_ = sv.sdo.SetSpeed(track, speedPcc, true)
 }

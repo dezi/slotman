f10652797e41098ca26616a03e9f55199855268f
dezi <dezi@kappa-mm.de>
2025-03-03 07:44:44 +0100

    Dual Uart.

--- soft/golang/slotman/things/nxp/sc16is752/const.go
+++ soft/golang/slotman/things/nxp/sc16is752/const.go
@@ -26,22 +26,22 @@ const (
 	// D:SDA
 	//
 
-	ThingI2CAddressAA byte = 0x90
-	ThingI2CAddressAB byte = 0x92
-	ThingI2CAddressAC byte = 0x94
-	ThingI2CAddressAD byte = 0x96
-	ThingI2CAddressBA byte = 0x98
-	ThingI2CAddressBB byte = 0x9A
-	ThingI2CAddressBC byte = 0x9C
-	ThingI2CAddressBD byte = 0x9E
-	ThingI2CAddressCA byte = 0xA0
-	ThingI2CAddressCB byte = 0xA2
-	ThingI2CAddressCC byte = 0xA4
-	ThingI2CAddressCD byte = 0xA6
-	ThingI2CAddressDA byte = 0xA8
-	ThingI2CAddressDB byte = 0xAA
-	ThingI2CAddressDC byte = 0xAC
-	ThingI2CAddressDD byte = 0xAE
+	ThingI2CAddressAA byte = 0x90>>1 ^ 1
+	ThingI2CAddressAB byte = 0x92>>1 ^ 1
+	ThingI2CAddressAC byte = 0x94>>1 ^ 1
+	ThingI2CAddressAD byte = 0x96>>1 ^ 1
+	ThingI2CAddressBA byte = 0x98>>1 ^ 1
+	ThingI2CAddressBB byte = 0x9A>>1 ^ 1
+	ThingI2CAddressBC byte = 0x9C>>1 ^ 1
+	ThingI2CAddressBD byte = 0x9E>>1 ^ 1
+	ThingI2CAddressCA byte = 0xA0>>1 ^ 1
+	ThingI2CAddressCB byte = 0xA2>>1 ^ 1
+	ThingI2CAddressCC byte = 0xA4>>1 ^ 1
+	ThingI2CAddressCD byte = 0xA6>>1 ^ 1
+	ThingI2CAddressDA byte = 0xA8>>1 ^ 1
+	ThingI2CAddressDB byte = 0xAA>>1 ^ 1
+	ThingI2CAddressDC byte = 0xAC>>1 ^ 1
+	ThingI2CAddressDD byte = 0xAE>>1 ^ 1
 
 	//
 	// Channel select
